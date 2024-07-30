4d3085fc49eea209fd0d8fff3b53dbdc526b73b5
RoMinjun <rominkleeman@hotmail.com>
2024-07-30 21:15:44 +0200

    added failsave for when app crashes

--- internal/youtube/youtube.go
+++ internal/youtube/youtube.go
@@ -1,7 +1,6 @@
 package youtube
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"sync"
@@ -17,7 +16,27 @@ var (
 	lastNotifiedId string
 )
 
+const lastNotifiedFilePath = "last_notified_id.txt"
+
+func saveLastNotifiedId() {
+	err := os.WriteFile(lastNotifiedFilePath, []byte(lastNotifiedId), 0644)
+	if err != nil {
+		log.Printf("Error saving last notified ID: %v", err)
+	}
+}
+
+func loadLastNotifiedId() {
+	data, err := os.ReadFile(lastNotifiedFilePath)
+	if err != nil {
+		log.Printf("Error loading last notified ID: %v", err)
+		return
+	}
+	lastNotifiedId = string(data)
+}
+
 func StartCheckingYouTube() {
+	loadLastNotifiedId()
+
 	go func() {
 		for {
 			time.Sleep(55 * time.Minute)
@@ -32,27 +51,28 @@ func StartCheckingYouTube() {
 
 	for {
 		checkMu.Lock()
-		newVideoId, videoTitle := checkForNewVideo()
+		newVideoId, videoTitle, channelTitle := checkForNewVideo()
 		if newVideoId != "" && newVideoId != lastNotifiedId {
 			lastNotifiedId = newVideoId
+			saveLastNotifiedId()
 			videoLink := "https://www.youtube.com/watch?v=" + newVideoId
-			message := "Hey @everyone, Coach Kirei just uploaded [" + videoTitle + "](" + videoLink + ")"
+			message := "Hey @everyone," + channelTitle + " just uploaded [" + videoTitle + "](" + videoLink + ")"
 			discord.SendDiscordWebhook(message)
-			fmt.Printf("New video found! URL: %s\n", videoLink)
+			log.Printf("New video found! URL: %s\n", videoLink)
 		}
 		checkMu.Unlock()
 		time.Sleep(15 * time.Minute) // Check every 15 minutes
 	}
 }
 
-func checkForNewVideo() (string, string) {
+func checkForNewVideo() (string, string, string) {
 	service.YouTubeMu.Lock()
 	defer service.YouTubeMu.Unlock()
 
 	channelId := os.Getenv("CHANNEL_ID")
 	if channelId == "" {
 		log.Printf("Error: CHANNEL_ID is empty")
-		return "", ""
+		return "", "", ""
 	}
 
 	call := service.YouTubeService.Search.List([]string{"snippet"}).ChannelId(channelId).Order("date").MaxResults(1)
@@ -63,20 +83,21 @@ func checkForNewVideo() (string, string) {
 				// Quota exceeded, backoff
 				log.Println("Quota exceeded, backing off for 30 mins")
 				time.Sleep(30 * time.Minute)
-				return "", ""
+				return "", "", ""
 			}
 		}
 		log.Printf("Error making API call: %v", err)
-		return "", ""
+		return "", "", ""
 	}
 
 	if len(response.Items) == 0 {
 		log.Println("No videos found")
-		return "", ""
+		return "", "", ""
 	}
 
 	video := response.Items[0]
 	videoId := video.Id.VideoId
 	videoTitle := video.Snippet.Title
-	return videoId, videoTitle
+	channelTitle := video.Snippet.ChannelTitle
+	return videoId, videoTitle, channelTitle
 }
